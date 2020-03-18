0d544cb2f6eff732042e0d3a27519cc7615a473a
Mauro <tik@lag.party>
2020-03-17 21:30:49 -0300

    Add config

---
+++ config.go
@@ -0,0 +1,19 @@
+package main
+
+type Configuration struct {
+	HTTPClient struct {
+		UserAgent string `yaml:"user-agent"`
+		Timeout   int    `yaml:"timeout"`
+	} `yaml:"http-client"`
+
+	HTTPServer struct {
+		Port string `yaml:"port"`
+	} `yaml:"http-server"`
+
+	Database struct {
+		Host     string `yaml:"host"`
+		Port     string `yaml:"port"`
+		Username string `yaml:"username"`
+		Password string `yaml:"password"`
+	} `yaml:"database"`
+}
--- main.go
+++ main.go
@@ -2,25 +2,51 @@ package main
 
 import (
 	"encoding/json"
+	"io/ioutil"
+	"log"
 	"net/http"
+
+	"gopkg.in/yaml.v2"
 )
 
-type Status struct {
+// Config holds the parsed config.yaml
+var Config *Configuration
+
+// StatusResponse contains JSON data about the server status
+type StatusResponse struct {
 	StatusCode int    `json:"status_code"`
 	StatusMsg  string `json:"status_msg"`
 }
 
-func main() {
-	// API entrypoints
-	http.HandleFunc("/status", status)
+// loadConfig opens and parses config.yaml
+func loadConfig() {
+	f, err := ioutil.ReadFile("config.yaml")
+	if err != nil {
+		log.Fatal("error opening config.yaml: %v", err)
+	}
 
-	http.ListenAndServe(":3000", nil)
+	cfg := Configuration{}
+	err = yaml.Unmarshal([]byte(f), &cfg)
+	if err != nil {
+		log.Fatalf("error parsing config.yaml: %v", err)
+	}
+	Config = &cfg
 }
 
 // Status is the entrypoint function for GET /status
-// Displays general information about the VarQ server
+// Shows general information about the VarQ server status
 func status(w http.ResponseWriter, r *http.Request) {
-	s := Status{StatusCode: 0, StatusMsg: "Online"}
+	s := StatusResponse{StatusCode: 0, StatusMsg: "online"}
 	out, _ := json.Marshal(s)
 	w.Write(out)
+}
+
+func main() {
+	loadConfig()
+
+	// REST API entrypoints
+	http.HandleFunc("/status", status)
+
+	log.Printf("Starting VarQ web server: http://127.0.0.1:%s/", Config.HTTPServer.Port)
+	http.ListenAndServe(":"+Config.HTTPServer.Port, nil)
 }
