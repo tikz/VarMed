bca7ed319cbe4c75d9179b987ae64a2272b723b1
Mauro <tik@lag.party>
2020-03-20 10:07:58 -0300

    Add Fpocket

--- binding/binding.go
+++ binding/binding.go
@@ -1,1 +1,20 @@
-// TODO
+package binding
+
+import (
+	"fmt"
+	"varq/binding/fpocket"
+	"varq/pdb"
+)
+
+type Binding struct {
+	Pockets []*fpocket.Pocket
+}
+
+func NewBinding(pdb *pdb.PDB) (*Binding, error) {
+	pockets, err := fpocket.Run(pdb)
+	if err != nil {
+		return nil, fmt.Errorf("running Fpocket: %v", err)
+	}
+
+	return &Binding{Pockets: pockets}, nil
+}
