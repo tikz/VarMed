9ce77caff50c3f6071da9722641cd4ba584d4548
Mauro Song <maurosong@protonmail.com>
2020-03-20 15:50:29 -0300

    Fix interaction

--- interaction/interaction.go
+++ interaction/interaction.go
@@ -1,64 +1,73 @@
 package interaction
 
 import (
-	"fmt"
 	"math"
-	"time"
 	"varq/pdb"
 )
 
+// InteractionAnalysis holds the collected data in the interaction analysis step
 type InteractionAnalysis struct {
 	Interactions []*AminoacidsInteraction
+	Error        error
 }
 
+// ChainsInteraction holds all aminoacid interactions between two chains
+type ChainsInteraction struct {
+	Chain1                 string
+	Chain2                 string
+	AminoacidsInteractions []*AminoacidsInteraction
+}
+
+// AminoacidsInteraction holds all interaction parameters between two aminoacids
 type AminoacidsInteraction struct {
 	Distance   float64
 	Aminoacid1 *pdb.Aminoacid
 	Aminoacid2 *pdb.Aminoacid
 }
 
+// RunInteractionAnalysis starts the interaction analysis step
+func RunInteractionAnalysis(p *pdb.PDB, results chan<- *InteractionAnalysis) {
+	// TODO: too trusting of the data sent by the external caller, do error checking around here.
+	results <- &InteractionAnalysis{Interactions: calculateChainsInteraction(p.Chains)}
+}
+
+// calculateDistance returns the distance between two atoms
 func calculateDistance(atom1 *pdb.Atom, atom2 *pdb.Atom) float64 {
 	return math.Sqrt(math.Pow(atom1.X-atom2.X, 2) + math.Pow(atom1.Y-atom2.Y, 2) + math.Pow(atom1.Z-atom2.Z, 2))
 }
 
-func calculateInteractions(chains map[string]map[int64]*pdb.Aminoacid) (interactions []*AminoacidsInteraction) {
-	start := time.Now()
-
+func calculateChainsInteraction(chains map[string]map[int64]*pdb.Aminoacid) (aaInteracts []*AminoacidsInteraction) {
 	var i1, i2 int
-	for _, chain1 := range chains {
-		for _, chain2 := range chains {
-			if i2 > i1 {
+
+	for chainName1, chain1 := range chains {
+		for chainName2, chain2 := range chains {
+			if i2 > i1 && chainName1 != chainName2 {
 				chainAtoms1 := flatten(chain1)
 				chainAtoms2 := flatten(chain2)
-				_ = calculateChainAAsDistance(chainAtoms1, chainAtoms2)
 
+				aaInteracts = calculateAminoacidsInteraction(chainAtoms1, chainAtoms2)
 			}
 			i2++
 		}
 		i1++
 	}
 
-	end := time.Since(start)
-	fmt.Println("finished in", end)
-	return interactions
+	return aaInteracts
 }
 
-func calculateChainAAsDistance(c1 []*pdb.Atom, c2 []*pdb.Atom) (ai []*AminoacidsInteraction) {
-	for i1, atom1 := range c1 {
-		for i2, atom2 := range c2 {
-			if i2 > i1 {
-				if dist := calculateDistance(atom1, atom2); dist < 5 {
-					fmt.Println(dist, atom1, atom2)
-					ai = append(ai, &AminoacidsInteraction{
-						Distance:   dist,
-						Aminoacid1: atom1.Aminoacid,
-						Aminoacid2: atom2.Aminoacid,
-					})
-				}
+func calculateAminoacidsInteraction(chain1 []*pdb.Atom, chain2 []*pdb.Atom) (aaInteracts []*AminoacidsInteraction) {
+	for _, atom1 := range chain1 {
+		for _, atom2 := range chain2 {
+			if dist := calculateDistance(atom1, atom2); dist < 5 {
+				aaInteracts = append(aaInteracts, &AminoacidsInteraction{
+					Distance:   dist,
+					Aminoacid1: atom1.Aminoacid,
+					Aminoacid2: atom2.Aminoacid,
+				})
 			}
 		}
 	}
-	return ai
+	return aaInteracts
 }
 
 // flatten converts the aminoacid slice datatype to a flat slice of atom pointers
@@ -69,8 +78,4 @@ func flatten(chain map[int64]*pdb.Aminoacid) (atoms []*pdb.Atom) {
 		}
 	}
 	return atoms
-}
-
-func NewInteraction(p *pdb.PDB) {
-	calculateInteractions(p.Chains)
 }
