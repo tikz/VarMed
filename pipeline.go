ec387ae7eb3f3f7b26d4f3c5964fbe10391937b0
Mauro Song <maurosong@protonmail.com>
2020-03-30 03:22:47 -0300

    Refactor

--- pipeline.go
+++ pipeline.go
@@ -16,87 +16,87 @@ import (
 // Analysis contains all pipeline steps results for a single PDB entry.
 type Analysis struct {
 	PDB         *pdb.PDB
-	Binding     *binding.BindingAnalysis
-	Interaction *interaction.InteractionAnalysis
-	Exposure    *exposure.ExposureAnalysis
+	Binding     *binding.Step
+	Interaction *interaction.Step
+	Exposure    *exposure.Step
 	Error       error `json:"-"`
 }
 
 // pipelinePDBWorker fetches a single PDB crystal file.
-func pipelinePDBWorker(pdbChan <-chan *pdb.PDB, analysisChan chan<- *Analysis) {
+func pipelinePDBWorker(pdbChan <-chan *pdb.PDB, aChan chan<- *Analysis) {
 	for crystal := range pdbChan {
-		crystal.Fetch()
-		if crystal.Error != nil {
-			analysisChan <- &Analysis{PDB: crystal, Error: fmt.Errorf("PDB %s: %v", crystal.ID, crystal.Error)}
+		analysis := Analysis{PDB: crystal}
+		err := crystal.Fetch()
+		if err != nil {
+			analysis.Error = fmt.Errorf("fetch PDB %s: %v", crystal.ID, err)
+			aChan <- &analysis
 			continue
 		}
 
-		analysisChan <- analysePDB(&Analysis{PDB: crystal})
+		err = crystal.Extract()
+		if err != nil {
+			analysis.Error = fmt.Errorf("extract PDB %s: %v", crystal.ID, err)
+			aChan <- &analysis
+			continue
+		}
+
+		aChan <- analysePDB(&analysis)
 	}
 }
 
 // analysePDB runs each available analysis in parallel for a single structure.
-func analysePDB(analysis *Analysis) *Analysis {
+func analysePDB(a *Analysis) *Analysis {
 	// Create temp PDB on filesystem for analysis with external tools
-	analysis.PDB.LocalFilename = "varq_" + analysis.PDB.ID
-	analysis.PDB.LocalPath = "/tmp/" + analysis.PDB.LocalFilename + ".pdb"
+	a.PDB.LocalFilename = "varq_" + a.PDB.ID
+	a.PDB.LocalPath = "/tmp/" + a.PDB.LocalFilename + ".pdb"
 
-	err := ioutil.WriteFile(analysis.PDB.LocalPath, analysis.PDB.RawPDB, 0644)
+	err := ioutil.WriteFile(a.PDB.LocalPath, a.PDB.RawPDB, 0644)
 	if err != nil {
-		analysis.Error = fmt.Errorf("create tmp PDB: %v", err)
-		return analysis
+		a.Error = fmt.Errorf("create tmp PDB: %v", err)
+		return a
 	}
 
 	defer func() {
-		os.Remove(analysis.PDB.LocalPath)
+		os.Remove(a.PDB.LocalPath)
 	}()
 
-	bindingChan := make(chan *binding.BindingAnalysis)
-	interactionChan := make(chan *interaction.InteractionAnalysis)
-	exposureChan := make(chan *exposure.ExposureAnalysis)
+	bindingChan := make(chan *binding.Step)
+	interactionChan := make(chan *interaction.Step)
+	exposureChan := make(chan *exposure.Step)
 
-	go binding.RunBindingAnalysis(analysis.PDB, bindingChan)
-	go interaction.RunInteractionAnalysis(analysis.PDB, interactionChan)
-	go exposure.RunExposureAnalysis(analysis.PDB, exposureChan)
+	go binding.RunBindingStep(a.PDB, bindingChan)
+	go interaction.RunInteractionStep(a.PDB, interactionChan)
+	go exposure.RunExposureStep(a.PDB, exposureChan)
 
-	// TODO: Maybe refactor these repeated patterns
+	// TODO: refactor these repeated patterns
 	bindingRes := <-bindingChan
 	if bindingRes.Error != nil {
-		analysis.Error = fmt.Errorf("binding analysis: %v", bindingRes.Error)
-		return analysis
+		a.Error = fmt.Errorf("binding analysis: %v", bindingRes.Error)
+		return a
 	}
-	analysis.Binding = bindingRes
-	log.Printf("PDB %s binding analysis done in %.3f secs", analysis.PDB.ID, bindingRes.Duration.Seconds())
+	a.Binding = bindingRes
+	log.Printf("PDB %s binding analysis done in %.3f secs", a.PDB.ID, bindingRes.Duration.Seconds())
 
 	interactionRes := <-interactionChan
 	if interactionRes.Error != nil {
-		analysis.Error = fmt.Errorf("interaction analysis: %v", interactionRes.Error)
-		return analysis
+		a.Error = fmt.Errorf("interaction analysis: %v", interactionRes.Error)
+		return a
 	}
-	analysis.Interaction = interactionRes
-	log.Printf("PDB %s interaction analysis done in %.3f secs", analysis.PDB.ID, interactionRes.Duration.Seconds())
+	a.Interaction = interactionRes
+	log.Printf("PDB %s interaction analysis done in %.3f secs", a.PDB.ID, interactionRes.Duration.Seconds())
 
 	exposureRes := <-exposureChan
 	if exposureRes.Error != nil {
-		analysis.Error = fmt.Errorf("exposure analysis: %v", exposureRes.Error)
-		return analysis
+		a.Error = fmt.Errorf("exposure analysis: %v", exposureRes.Error)
+		return a
 	}
-	analysis.Exposure = exposureRes
-	log.Printf("PDB %s exposure analysis done in %.3f secs", analysis.PDB.ID, exposureRes.Duration.Seconds())
+	a.Exposure = exposureRes
+	log.Printf("PDB %s exposure analysis done in %.3f secs", a.PDB.ID, exposureRes.Duration.Seconds())
 
-	// for _, pocket := range analysis.Binding.Pockets {
-	// 	for _, res := range pocket.Residues {
-	// 		fmt.Println(res.Abbrv1, res)
-	// 	}
-	// }
-
-	// for _, res := range analysis.Binding.Catalytic.Residues {
-	// 	fmt.Println(res)
-	// }
-	return analysis
+	return a
 }
 
-func RunPipelineCrystals(pdbs []*pdb.PDB) (analyses []*Analysis, err error) {
+func runPipelinePDBs(pdbs []*pdb.PDB) (analyses []*Analysis, err error) {
 	length := len(pdbs)
 	pdbChan := make(chan *pdb.PDB, length)
 	analysisChan := make(chan *Analysis, length)
@@ -134,37 +134,13 @@ func RunPipeline(uniprotID string, filterPDBIDs []string) ([]*Analysis, error) {
 	}
 
 	if len(filterPDBIDs) > 0 {
-		u.FilterCrystals(filterPDBIDs)
+		u.FilterPDBs(filterPDBIDs)
 	}
 
-	analyses, err := RunPipelineCrystals(u.Crystals)
+	analyses, err := runPipelinePDBs(u.PDBs)
 	if err != nil {
 		return nil, fmt.Errorf("analyzing crystals: %v", err)
 	}
-
-	// for _, crystal := range u.Crystals {
-	// 	if _, ok := crystal.SIFTS.UniProtIDs[u.ID].Chains["A"]; ok {
-	// 		fmt.Println(u.ID, crystal.ID)
-	// 		fmt.Println(crystal.SIFTS.UniProtIDs)
-	// 		fmt.Println("UNIPROT", u.Sequence[crystal.SIFTS.UniProtIDs[u.ID].Chains["A"].UniProtStart-1:crystal.SIFTS.UniProtIDs[u.ID].Chains["A"].UniProtEnd])
-	// 		fmt.Print("SEQRES  ")
-	// 		start := crystal.SIFTS.UniProtIDs[u.ID].Chains["A"].PDBStart - 1
-	// 		for i := start; i < int64(len(crystal.SeqRes["A"])); i++ {
-	// 			fmt.Print(crystal.SeqRes["A"][i].Abbrv1)
-	// 		}
-	// 		fmt.Println()
-	// 		fmt.Print("PDB     ")
-	// 		for i := 1; i < len(crystal.Chains["A"]); i++ {
-	// 			if pos, ok := crystal.Chains["A"][int64(i)-crystal.ChainsOffsets["A"]-1]; ok {
-	// 				fmt.Print(pos.Abbrv1)
-	// 			} else {
-	// 				fmt.Print(" ")
-	// 			}
-	// 		}
-	// 		fmt.Println()
-	// 	}
-
-	// }
 
 	end := time.Since(start)
 	log.Printf("Finished UniProt %s in %.3f secs", u.ID, end.Seconds())
