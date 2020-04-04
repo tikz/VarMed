449c659e5694f6217927d48644580234f1959442
Mauro <tik@lag.party>
2020-04-04 02:20:27 -0300

    Add PDBbind and ligand distance to residues

--- binding/binding.go
+++ binding/binding.go
@@ -4,14 +4,16 @@ import (
 	"fmt"
 	"time"
 	"varq/binding/fpocket"
+	"varq/binding/ligand"
 	"varq/binding/mcsa"
 	"varq/pdb"
 )
 
 // BindingAnalysis holds the collected data in the binding analysis step
 type Step struct {
-	Pockets   []*fpocket.Pocket // Only pockets with drug score >0.5
-	Catalytic *mcsa.Catalytic
+	Pockets   []*fpocket.Pocket         // pockets with Fpocket drug score of >0.5
+	Catalytic *mcsa.Catalytic           // catalytic residues in M-CSA
+	Ligands   map[string][]*pdb.Residue // ligand ID to near residues
 	Duration  time.Duration
 	Error     error
 }
@@ -24,10 +26,20 @@ func RunBindingStep(pdb *pdb.PDB, results chan<- *Step) {
 		results <- &Step{Error: fmt.Errorf("running Fpocket: %v", err)}
 	}
 
-	csa, err := mcsa.GetCSA(pdb)
+	csa, err := mcsa.GetPositions(pdb)
 	if err != nil {
 		results <- &Step{Error: fmt.Errorf("M-CSA: %v", err)}
 	}
 
-	results <- &Step{Pockets: pockets, Catalytic: csa, Duration: time.Since(start)}
+	ligand, err := ligand.ResiduesNearLigands(pdb)
+	if err != nil {
+		results <- &Step{Error: fmt.Errorf("Ligands: %v", err)}
+	}
+
+	results <- &Step{
+		Pockets:   pockets,
+		Catalytic: csa,
+		Ligands:   ligand,
+		Duration:  time.Since(start),
+	}
 }
---
+++ binding/ligand/ligand.go
@@ -0,0 +1,52 @@
+package ligand
+
+import (
+	"fmt"
+	"math"
+	"os"
+	"varq/pdb"
+)
+
+// calculateDistance returns the distance between two atoms
+func calculateDistance(atom1 *pdb.Atom, atom2 *pdb.Atom) float64 {
+	return math.Sqrt(math.Pow(atom1.X-atom2.X, 2) + math.Pow(atom1.Y-atom2.Y, 2) + math.Pow(atom1.Z-atom2.Z, 2))
+}
+
+// ResiduesNearLigands returns a map of ligand IDs to near residues.
+func ResiduesNearLigands(p *pdb.PDB) (map[string][]*pdb.Residue, error) {
+	// TODO: just load one time, somewhere
+	pdbBind, err := LoadPDBBind()
+	if err != nil {
+		return nil, fmt.Errorf("cannot load PDBBind data: %v", err)
+	}
+
+	ligands := make(map[string][]*pdb.Residue)
+
+	for _, hetatm := range p.HetAtoms {
+		ln := hetatm.Residue
+
+		// ignore ligand IDs not present in PDBBind
+		if _, exists := pdbBind[ln]; !exists {
+			continue
+		}
+
+		for _, chain := range p.Chains {
+			for _, res := range chain {
+				var hasCloseAtom bool
+				var i int
+
+				for !hasCloseAtom && i < len(res.Atoms) {
+					hasCloseAtom = calculateDistance(hetatm, res.Atoms[i]) < 5
+					i++
+				}
+
+				if hasCloseAtom && (len(ligands[ln]) == 0 || ligands[ln][len(ligands[ln])-1] != res) {
+					ligands[ln] = append(ligands[ln], res)
+				}
+
+			}
+		}
+	}
+	os.Exit(0)
+	return ligands, nil
+}
