4f5823db60a927846eaa8b01f9d99cc4f1abc7f7
Mauro Song <maurosong@protonmail.com>
2020-04-07 23:17:30 -0300

    Add events between LiteMol and FeatureViewer

--- web/src/components/SequenceViewer.jsx
+++ web/src/components/SequenceViewer.jsx
@@ -1,45 +1,88 @@
 import React from 'react';
 
 export default class SequenceViewer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            pos: 0
+        };
+
+        this.handleMouseMove = this.handleMouseMove.bind(this);
+        this.handleMouseLeave = this.handleMouseLeave.bind(this);
+    }
+
+    // This is hackish and adds overhead, but FeatureViewer doesn't provide mousemove or
+    // position change events and very little in the API is exported. In the future, maybe
+    // ask the authors on GitHub for this feature, or fork the repo and add it.
+    handleMouseMove(e) {
+        var posText = this.state.zoomPositionElement.innerText;
+        if (posText != this.state.pos) {
+            var pos = parseInt(posText.slice(0, posText.length - 1));
+            this.props.highlight(pos, pos);
+            this.setState(state => ({
+                pos: posText
+            }));
+        }
+    };
+
+    handleMouseLeave(e) {
+        this.props.highlight(0, 0);
+    };
+
     componentDidMount() {
         var FeatureViewer = require("feature-viewer");
-        var ft2 = new FeatureViewer("FDSJKLFJDSFKLJDFHADJKLFHDSJKLFHDAFJKLDHFJKLDASFHDJKLFHDSAJKLFHDAKLFJDHSAFKLDLSNCDJKLFENFIUPERWDJKPCNVDFPIEHFDCFJDKOWFPDJWFKLXSJFDW9FIPUAENDCXAMSFNDUAFIDJFDLKSAFJDSAKFLJDSADJFDW9FIPUAENDCXAMSFNDAAAAAAAAAAAFJDSAKFL", "#seq", {
+        var fv = new FeatureViewer("MTEYKLVVVGAGGVGKSALTIQLIQNHFVDEYDPTIEDSYRKQVVIDGETCLLDILDTAGQEEYSAMRDQYMRTGEGFLCVFAINNSKSFADINLYREQIKRVKDSDDVPMVLVGNKCDLPTRTVDTKQAHELAKSYGIPFIETSAKTRQGVEDAFYTLVREIRQYRMKKLNSSDDGTQGCMGLPCVVM", "#fv", {
             showAxis: true,
             showSequence: true,
             brushActive: true,
             toolbar: true,
-            bubbleHelp: true,
-            zoomMax: 10
+            bubbleHelp: false,
+            zoomMax: 20
         });
 
-        ft2.addFeature({
-            data: [{ x: 20, y: 40 }, { x: 46, y: 100 }, { x: 123, y: 167 }],
-            name: "1B93",
+        fv.addFeature({
+            data: [{ x: 1, y: 168, description: "Chain A" }],
+            name: "3CON",
             className: "test1",
-            color: "#005572",
+            color: "#2196F3",
             type: "rect",
             filter: "type1"
         });
-        ft2.addFeature({
-            data: [{ x: 50, y: 90, description: "PF666" }],
+        fv.addFeature({
+            data: [{ x: 5, y: 165, description: "PF00071 Ras" }],
             name: "Pfam",
             className: "test6",
-            color: "#81BEAA",
+            color: "#2196F3",
             type: "rect",
             filter: "type2"
         });
-        ft2.addFeature({
+        fv.addFeature({
             data: [{ x: 52, y: 52 }, { x: 92, y: 92 }],
-            name: "Variations",
-            className: "test2",
-            color: "#006588",
+            name: "Variant",
+            className: "variant",
+            color: "#21CBF3",
             type: "rect",
-            filter: "type2"
+            filter: "type3"
         });
-    }
+
+        var selectFunc = this.props.select
+        fv.onFeatureSelected(function (d) {
+            console.log(d);
+            selectFunc(d.detail.start, d.detail.end);
+        });
+
+        fv.onZoom(function (d) {
+            console.log(d.detail);
+        });
+
+        this.setState(state => ({
+            zoomPositionElement: document.getElementById("zoomPosition")
+        }));
+
+    };
     render() {
         return (
-            <div id="seq" />
+            <div id="fv" onMouseMove={this.handleMouseMove} onMouseLeave={this.handleMouseLeave} />
         )
-    }
+    };
 }
