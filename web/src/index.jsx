f4b7d017a0feac42380fd034ccafccf2bcf54dfb
Mauro Song <maurosong@protonmail.com>
2020-04-08 12:14:12 -0300

    Fix build paths. Add favicons.

--- web/src/index.jsx
+++ web/src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import Results from './components/Results';
 import './styles/app.scss';
 
+require('file-loader?name=[name].[ext]!./index.html');
 
 ReactDOM.render(
     <Results />,
