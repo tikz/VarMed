90e2bbbd03432cf70712821de3060f602b2234fa
Mauro <tik@lag.party>
2020-04-13 23:50:52 -0300

    Remove repeated code

--- main.go
+++ main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"encoding/gob"
 	"flag"
 	"log"
-	"os"
 	"varq/config"
 	"varq/http"
 )
@@ -36,24 +34,4 @@ func main() {
 	} else {
 		httpServe()
 	}
-}
-
-func writeGob(filePath string, object interface{}) error {
-	file, err := os.Create(filePath)
-	if err == nil {
-		encoder := gob.NewEncoder(file)
-		encoder.Encode(object)
-	}
-	file.Close()
-	return err
-}
-
-func readGob(filePath string, object interface{}) error {
-	file, err := os.Open(filePath)
-	if err == nil {
-		decoder := gob.NewDecoder(file)
-		err = decoder.Decode(object)
-	}
-	file.Close()
-	return err
 }
