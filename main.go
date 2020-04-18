f888beaa872f75c797f326eefdbe4b7dc8dfdd60
Mauro <tik@lag.party>
2020-04-18 19:30:01 -0300

    Add print

--- main.go
+++ main.go
@@ -40,6 +40,7 @@ func main() {
 func makeSampleResults() {
 	_, err := os.Stat("data/jobs/fe2423053f1a75a300e4074b1609ed972e3e2eaeae149f21d9c5fd79b4ef3d5c.varq")
 	if os.IsNotExist(err) {
+		log.Println("Running pipeline to populate sample results...")
 		j := NewJob(&JobRequest{UniProtID: "P00390", PDBIDs: []string{"2GH5"}})
 		j.Process()
 	}
