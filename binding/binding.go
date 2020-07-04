791a1cd960b116b951a288387d4fe83e4dc6bdab
Mauro Song <maurosong@protonmail.com>
2020-07-04 18:56:09 -0300

    Remove MCSA

--- binding/binding.go
+++ binding/binding.go
@@ -5,18 +5,16 @@ import (
 	"time"
 	"varq/binding/fpocket"
 	"varq/binding/ligand"
-	"varq/binding/mcsa"
 	"varq/pdb"
 	"varq/uniprot"
 )
 
 // Results holds the collected data in the binding analysis step
 type Results struct {
-	Pockets   []*fpocket.Pocket         `json:"pockets"`   // pockets with Fpocket drug score of >0.5
-	Catalytic *mcsa.Catalytic           `json:"catalytic"` // catalytic residues in M-CSA
-	Ligands   map[string][]*pdb.Residue `json:"ligands"`   // ligand ID to near residues
-	Duration  time.Duration             `json:"duration"`
-	Error     error                     `json:"error"`
+	Pockets  []*fpocket.Pocket         `json:"pockets"` // pockets with Fpocket drug score of >0.5
+	Ligands  map[string][]*pdb.Residue `json:"ligands"` // ligand ID to near residues
+	Duration time.Duration             `json:"duration"`
+	Error    error                     `json:"error"`
 }
 
 // Run starts the binding analysis step
@@ -27,20 +25,14 @@ func Run(unp *uniprot.UniProt, pdb *pdb.PDB, results chan<- *Results, msg func(s
 		results <- &Results{Error: fmt.Errorf("running Fpocket: %v", err)}
 	}
 
-	csa, err := mcsa.GetPositions(unp, pdb, msg)
-	if err != nil {
-		results <- &Results{Error: fmt.Errorf("M-CSA: %v", err)}
-	}
-
 	ligand, err := ligand.ResiduesNearLigands(pdb, msg)
 	if err != nil {
 		results <- &Results{Error: fmt.Errorf("Ligands: %v", err)}
 	}
 
 	results <- &Results{
-		Pockets:   pockets,
-		Catalytic: csa,
-		Ligands:   ligand,
-		Duration:  time.Since(start),
+		Pockets:  pockets,
+		Ligands:  ligand,
+		Duration: time.Since(start),
 	}
 }
