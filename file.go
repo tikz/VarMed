d10fc83de5d849fc3596db8d32dafe4ac8dc5e44
Mauro <tik@lag.party>
2020-10-07 15:54:00 -0300

    Remove comments

--- file.go
+++ file.go
@@ -33,62 +33,6 @@ func loadJob(id string) (*Job, error) {
 	return &j, nil
 }
 
-// func loadPDB(pdbID string) (*pdb.PDB, error) {
-// 	path := pdbDir + pdbID + fileExt
-// 	_, err := os.Stat(path)
-// 	if os.IsNotExist(err) {
-// 		p, err := pdb.NewPDBFromID(pdbID)
-// 		if err != nil {
-// 			return nil, err
-// 		}
-
-// 		err = write(path, &p)
-// 		if err != nil {
-// 			return nil, fmt.Errorf("write PDB: %v", err)
-// 		}
-// 		return &p, nil
-// 	}
-
-// 	return readPDB(pdbID)
-// }
-
-// func readPDB(pdbID string) (*pdb.PDB, error) {
-// 	path := pdbDir + pdbID + fileExt
-// 	p := new(pdb.PDB)
-// 	err := read(path, &p)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("load file: %v", err)
-// 	}
-
-// 	err = p.Parse()
-// 	return p, err
-// }
-
-// func loadUniProt(unpID string) (*uniprot.UniProt, error) {
-// 	path := unpDir + unpID + fileExt
-// 	_, err := os.Stat(path)
-// 	if os.IsNotExist(err) {
-// 		u, err := uniprot.NewUniProt(unpID)
-// 		if err != nil {
-// 			return nil, err
-// 		}
-
-// 		err = write(path, &u)
-// 		if err != nil {
-// 			return nil, fmt.Errorf("write UniProt: %v", err)
-// 		}
-// 		return u, nil
-// 	}
-
-// 	u := new(uniprot.UniProt)
-// 	err = read(path, &u)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("load file: %v", err)
-// 	}
-
-// 	return u, nil
-// }
-
 func write(filePath string, object interface{}) error {
 	file, err := os.Create(filePath)
 	if err == nil {
